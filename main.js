95035c998246568559320c02e315b25aeeee05b1
didar <dider.alam115@gmail.com>
2021-08-07 00:45:43 +0600

    practice 1

---
+++ main.js
@@ -0,0 +1,49 @@
+
+// Fibonacci series 
+/*function fibonacciSeries(num) {
+  let a = 0;
+  let b = 1;
+  let temp = 0;
+
+  for (let i = 1; i <= num; i++){
+
+    if (i == 1) {
+      console.log(a); //0
+    }
+    if (i == 2) {
+      console.log(b); //1
+    }
+
+
+    temp = a + b; //0+1=1
+    a = b;   //a=1
+    b = temp; 
+    console.log(temp);
+
+   
+    //0,1,1,2,3,5,8,13,21
+
+    
+  }
+  
+
+}
+
+var num = 10;
+console.log(fibonacciSeries(num)); */
+
+
+//Basic Object 
+/*
+var rahim = {
+	fullName: "Rahim Miya",
+	age: 21,
+	address: "Dhaka",
+	job: "Job Holder",
+	welcomeMsg: function () {
+		console.log("Hello There");
+	},
+};
+console.log(rahim.fullName);   */
+
+
