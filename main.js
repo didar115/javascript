06141631b7dc46022a0b7bf4854b3973d4752322
didar <dider.alam115@gmail.com>
2021-08-23 22:32:00 +0600

    reduce extra code & put into function

--- main.js
+++ main.js
@@ -230,42 +230,53 @@ function getInputValue(fieldId) {
 	const inputField = document.getElementById(fieldId);
 	const inputFieldInText = inputField.value;
 	const inputValue = parseFloat(inputFieldInText);
-	inputField.value = "";
+	document.getElementById(fieldId).value = "";
 	return inputValue;
 }
 
 function updateBankAmount(amount, fieldId) {
-	const updateField = document.getElementById(fieldId);
-	const updateFieldInText = updateField.innerText;
-	const previousValue = parseFloat(updateFieldInText);
-	const updateAmount = previousValue + amount;
-	updateField.innerText = updateAmount;
+	// const previousValue = getInnerText(fieldId);
+	// const updateAmount = previousValue + amount;
+	const depositValue = getInnerText('deposit-update');
+	const withDrawValue = getInnerText('withdraw-update');
+	const updateAmount = depositValue - withDrawValue + 1200;
+
+	document.getElementById(fieldId).innerText = updateAmount;
+}
+
+function getInnerText(fieldId) {
+	const hasTag = document.getElementById(fieldId);
+	const valueInText = hasTag.innerText;
+	const value = parseFloat(valueInText);
+	return value;
 }
 function updateBankAmountTotal(amount, isAdding) {
-	const getTag = document.getElementById("balance-update");
-	const valueInText = getTag.innerText;
-	const previosBanlace = parseFloat(valueInText);
+	const previosBanlace = getInnerText("balance-update");
 	let updateBalance;
 	if (isAdding == true) {
 		updateBalance = previosBanlace + amount;
 	} else {
 		updateBalance = previosBanlace - amount;
 	}
-
-	getTag.innerText = updateBalance;
+	document.getElementById("balance-update").innerText = updateBalance;
 }
 
 document
 	.getElementById("deposit-button")
 	.addEventListener("click", function () {
 		const amount = getInputValue("deposit-value");
-		const updatedAmount = updateBankAmount(amount, "deposit-update");
-		updateBankAmountTotal(amount, true);
+		if (amount > 0) {
+			const updatedAmount = updateBankAmount(amount, "deposit-update");
+			updateBankAmountTotal(amount, true);
+		}
 	});
 document
 	.getElementById("withdraw-button")
 	.addEventListener("click", function () {
 		const amount = getInputValue("withdraw-value");
-		const updatedAmount = updateBankAmount(amount, "withdraw-update");
-		updateBankAmountTotal(amount, false);
+		const balance = getInnerText("balance-update");
+		if (amount > 0 && amount <= balance) {
+			const updatedAmount = updateBankAmount(amount, "withdraw-update");
+			updateBankAmountTotal(amount, false);
+		}
 	});
